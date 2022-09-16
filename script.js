c9dc843b801906bd13822e3a1fd744193e5fb8c3
ivan bonilla <ivanbonilla02@yahoo.com>
2022-09-16 17:07:05 -0500

    added for loop and got password to start working just need to fix bug where false critria breaks the password generator

--- script.js
+++ script.js
@@ -3,33 +3,41 @@ var generateBtn = document.querySelector("#generate");
 
 // Global variables
 
-var upperCase=["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lowerCase=["abcdefghijklmnopqrstuvwxyz"];
-var numbers= ["123456789"];
+var upperCase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","j","w","x","y","z"];
+var numbers= ["1","2","3","4","5","6","7","8","9"];
 var special=["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
-
+// console.log(this)
 // need a function for length of password 
 // need function to be between 8-128 and must be returned to top of function
 // then function for other yes or no promts to build critria
+// debug length
+// parseint was messing up length for some reason
 // 
 
 function lengthOfPassword(){
-  var characters=parseInt( prompt("How many characters, must be between 8-128 characters.") );
-  console.log(characters)
+  
+  var characters=( prompt("How many characters, must be between 8-128 characters.") );
+
   if (!characters){
-        alert("Input correct value");
+        if(confirm("enter valid number")) lengthOfPassword();
       } else if(characters < 8 || characters> 128){
-        alert("Must be betweeen 8 and 128");
+        if(confirm("enter valid number")) lengthOfPassword();
       } else if (characters >= 8 || characters <= 128);
   
-  return parseInt(lengthOfPassword);
+  return characters;
 }
+
+
 
 //  need to have have promt send yes or no info to data and run it through
 //  for loop to create password from given true or false critria
+//  dont forget need length in criatia for passowrd 
 
-function critriaPass(){
+function generatePassword(){
+  var lengthPass= lengthOfPassword();
+
   var useUpperCaseLtrs = confirm("Do you want upper case letters in the password?");
   console.log(useUpperCaseLtrs)
   var useLowerCaseLtrs = confirm("Do you want lower case letters in the password?");
@@ -39,6 +47,11 @@ function critriaPass(){
   var  useSpecial = confirm("Do you want to use special characters in the password?")
   console.log(useSpecial)
 
+  if (!useLowerCaseLtrs && !useUpperCaseLtrs && !useNumbers && !useSpecial){
+       if(confirm("Sorry, please select at last one critria")) generatePassword();
+  } 
+
+
   var promptData=[];
 
   if (useUpperCaseLtrs){
@@ -47,67 +60,31 @@ function critriaPass(){
     promptData.push(...lowerCase);
   } if (useNumbers){
     promptData.push(...numbers);
-  }if (useSpecial){
+  } if (useSpecial){
     promptData.push(...special);
-  }
- 
+  } 
+
+  
+  
+  var password = "";
+     
+  console.log(promptData);
+  // console.log(password);
+
+  // this loop takes the a random character from data eact time
+
+  for(var i=0; i< lengthPass; i++) {
+    password += promptData [Math.floor(Math.random() * promptData.length)];
+  } 
   
 
-
-
-
-
-
+  
+ 
+  return password;
 
 
 
 }
-
-
-
-
-
-
-// // function for if person wants uppercase in password
-// function useUpperCase(){
-//   useUpperCaseLtrs = confirm("Do you want upper case letters in the password?");
-//   console.log(useUpperCaseLtrs)
-//   if (useUpperCaseLtrs && finalPassword < numOfCharacters){
-//     var characters = Math.floor(Math.random()* useUpperCaseLtrs.length)
-//     finalPassword = finalPassword + useUpperCaseLtrs[characters]
-//   }
-// }
-
-// // function for if person wants lowercase in password
-// function useLowerCase(){
-//   useLowerCaseLtrs = confirm("Do you want lower case letters in the password?");
-//   console.log(useLowerCaseLtrs)
-// // function for if person wants numbers in password
-// }
-// function numbers(){
-//   useNumbers = confirm("Do you want to use numbers in the password?");
-//   console.log(useNumbers)
-// }
-// // function for if person wants special characters in password
-// function special(){
-//   useSpecial = confirm("Do you want to use special characters in the password?")
-//   console.log(useSpecial)
-// }
-
-
-function generatePassword() {
-  console.log("start here");
-  lengthOfPassword();
-  critriaPass();
-
-
-
-  return "finalpassword"
-}
-
-
-
-
 
 
 
